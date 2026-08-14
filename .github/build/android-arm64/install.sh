799b729ae1e9f8611bb9cf4e5bfdfdc976718cdc
Genouka <142009020+Genouka@users.noreply.github.com>
2026-08-14 19:43:34 +0800

    Use

--- .github/build/android-arm64/install.sh
+++ .github/build/android-arm64/install.sh
@@ -3,20 +3,20 @@ set -e
 
 export DEBIAN_FRONTEND=noninteractive
 
-apt-get update -y
+sudo apt-get update -y
 
-apt-get install -y autoconf autopoint curl gettext git gperf libtool nasm pkg-config python python3-pip ragel texinfo unzip zip
+sudo apt-get install -y autoconf autopoint curl gettext git gperf libtool nasm pkg-config python python3-pip ragel texinfo unzip zip
 
-pip3 install --upgrade pip
-pip3 install cmake==4.3.4
-pip3 install meson==1.11.1
-pip3 install ninja==1.13.0
+sudo pip3 install --upgrade pip
+sudo pip3 install cmake==4.3.4
+sudo pip3 install meson==1.11.1
+sudo pip3 install ninja==1.13.0
 
 # Install Android NDK
 export ANDROID_NDK_VERSION="r28"
 if [ ! -d "/opt/android-ndk" ]; then
-  curl -sSL -o /tmp/android-ndk.zip "https://dl.google.com/android/repository/android-ndk-$ANDROID_NDK_VERSION-linux.zip"
-  unzip -q /tmp/android-ndk.zip -d /opt
-  mv /opt/android-ndk-$ANDROID_NDK_VERSION /opt/android-ndk
-  rm -f /tmp/android-ndk.zip
+  sudo curl -sSL -o /tmp/android-ndk.zip "https://dl.google.com/android/repository/android-ndk-$ANDROID_NDK_VERSION-linux.zip"
+  sudo unzip -q /tmp/android-ndk.zip -d /opt
+  sudo mv /opt/android-ndk-$ANDROID_NDK_VERSION /opt/android-ndk
+  sudo rm -f /tmp/android-ndk.zip
 fi
